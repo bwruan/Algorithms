690a4e4930467b0426bfdadeb2b075a358836547
Becky <becky.ruan524@gmail.com>
2021-09-23 20:28:42 -0700

    rotated string, sum of two large string numbers

---
+++ PracticeAlgorithms/Strings/Algos/RotationString.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strings.Algos
+{
+    public class RotationString
+    {
+        public bool IsRotatedTwoPlaces(string a, string b)
+        {
+            //check length
+            if (a.Length != b.Length)
+            {
+                return false;
+            }
+
+            if (a.Length < 2)
+            {
+                return a.Equals(b);
+            }
+
+            string clock_rot = "";
+            string anticlock_rot = "";
+            int len = b.Length;
+
+            // Initialize string as anti-clockwise rotation
+            anticlock_rot
+                = anticlock_rot + b.Substring(len - 2, len - (len - 2)) + b.Substring(0, len - 2);
+
+            // Initialize string as clock wise rotation
+            clock_rot = clock_rot + b.Substring(2) + b.Substring(0, 2);
+
+            // check if any of them is equal to string1
+            return (a.Equals(clock_rot) || a.Equals(anticlock_rot));
+        }
+    }
+}
---
+++ PracticeAlgorithms/Strings/Algos/SumTwoLargeStringNum.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strings.Algos
+{
+    public class SumTwoLargeStringNum
+    {
+        public string Sum(string a, string b)
+        {
+            // ensure length of second string is larger
+            if (a.Length > b.Length)
+            {
+                string temp = a;
+                a = b;
+                b = temp;
+            }
+
+            // Take an empty string for storing result
+            string str = "";
+
+            // Calculate length of both string
+            int n1 = a.Length;
+            int n2 = b.Length;
+            int diff = n2 - n1;
+
+            // Initially take carry zero
+            int carry = 0;
+
+            // Traverse from end of both strings
+            for (int i = n1 - 1; i >= 0; i--)
+            {
+                //compute sum of current digits and carry
+                int sum = ((int)(a[i] - '0') + (int)(b[i + diff] - '0') + carry);
+                str += (char)(sum % 10 + '0');
+                carry = sum / 10;
+            }
+
+            // Add remaining digits of b[]
+            for (int i = n2 - n1 - 1; i >= 0; i--)
+            {
+                int sum = ((int)(b[i] - '0') + carry);
+                str += (char)(sum % 10 + '0');
+                carry = sum / 10;
+            }
+
+            // Add remaining carry
+            if (carry > 0)
+            {
+                str += (char)(carry + '0');
+            }
+
+            // reverse resultant string
+            char[] ch2 = str.ToCharArray();
+            Array.Reverse(ch2);
+            return new string(ch2);
+        }
+    }
+}
