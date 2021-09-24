a26d69e405e7a3c61818ea20e8db98062e21d71e
Becky <becky.ruan524@gmail.com>
2021-09-23 19:41:05 -0700

    equal points between brackets and isomorphic

---
+++ PracticeAlgorithms/Strings/Algos/EqualPointBrackets.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strings.Algos
+{
+    public class EqualPointBrackets
+    {
+        public int FindIndexOfEqualBrackets(string s)
+        {
+            int len = s.Length;
+            int[] open = new int[len + 1];
+            int[] close = new int[len + 1];
+            int index = -1;
+
+            open[0] = 0;
+            close[len] = 0;
+            if (s[0] == '(')
+            {
+                open[1] = 1;
+            }
+            if (s[len - 1] == ')')
+            {
+                close[len - 1] = 1;
+            }
+
+            // store the number of opening brackets at each index
+            for (int i = 1; i < len; i++)
+            {
+                if (s[i] == '(')
+                {
+                    open[i + 1] = open[i] + 1;
+                }
+                else
+                {
+                    open[i + 1] = open[i];
+                }
+            }
+
+            // Store the number of closing brackets at each index
+            for (int i = len - 2; i >= 0; i--)
+            {
+                if (s[i] == ')')
+                {
+                    close[i] = close[i + 1] + 1;
+                }
+                else
+                {
+                    close[i] = close[i + 1];
+                }
+            }
+
+            // check if there is no opening or closing brackets
+            if (open[len] == 0 || close[0] == 0)
+            {
+                return len;
+            }
+
+            // check if there is any index at which both brackets are equal
+            for (int i = 0; i <= len; i++)
+            {
+                if (open[i] == close[i])
+                {
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+    }
+}
---
+++ PracticeAlgorithms/Strings/Algos/Isomorphs.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strings.Algos
+{
+    public class Isomorphs
+    {
+        public bool AreIsomorphic(string a, string b)
+        {
+            //lenght of two strings must be same to be considered isomorphic
+            if(a.Length != b.Length)
+            {
+                return false;
+            }
+
+			//use dictionary to store mappings of a chars to b chars.
+			var map = new Dictionary<char, char>();
+
+			for (int i = 0; i < a.Length; i++)
+			{
+				char c1 = a[i];
+				char c2 = b[i];
+
+				//if c1 has been encountered before
+				if (map.ContainsKey(c1))
+				{
+					//return false if first occurrence of c1 is mapped to a different character
+					if (map[c1] != c2)
+					{
+						return false;
+					}
+				}
+
+				//if c1 is encountered for the first time, 
+				else
+				{
+					//add c1 to dictionary and c2 as its value & continue looping
+					map.Add(c1, c2);
+				}
+			}
+
+			return true;
+		}
+    }
+}
