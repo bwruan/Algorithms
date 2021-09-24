5abf240500cc2964168fb4a1c52ec2d65caea96d
Becky <becky.ruan524@gmail.com>
2021-09-23 20:52:19 -0700

    min num repetition, nth occurrence

---
+++ PracticeAlgorithms/Strings/Algos/MinNumRepitition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strings.Algos
+{
+    public class MinNumRepitition
+    {
+        public int MinimumRepitition(string a, string b)
+        {
+
+        }
+    }
+}
