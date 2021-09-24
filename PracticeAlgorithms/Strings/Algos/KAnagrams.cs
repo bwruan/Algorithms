8aa1df84857a9a8a9231b1be3c5722216a9a8f0c
Becky <becky.ruan524@gmail.com>
2021-09-23 20:18:02 -0700

    pangram, k-anagram

---
+++ PracticeAlgorithms/Strings/Algos/KAnagrams.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strings.Algos
+{
+    public class KAnagrams
+    {
+        public bool AreKAnagrams(string a, string b, int k)
+        {
+            //check if strings are same length
+            if(a.Length != b.Length)
+            {
+                return false;
+            }
+
+            int[] hash_a = new int[26];
+
+            //store the occurrence of all characters in a hash_array
+            for (int i = 0; i < a.Length; i++)
+            {
+                hash_a[a[i] - 'a']++;
+            }
+
+            //store the occurrence of all characters in a hash_array
+            int count = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (hash_a[b[i] - 'a'] > 0)
+                {
+                    hash_a[b[i] - 'a']--;
+                }
+                else
+                {
+                    count++;
+                }
+
+                if (count > k)
+                {
+                    return false;
+                }
+            }
+
+            //return true if count is less than or equal to k
+            return true;
+        }
+    }
+}
---
+++ PracticeAlgorithms/Strings/Algos/Pangram.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strings.Algos
+{
+    public class Pangram
+    {
+        public bool IsPangram(string s)
+        {
+            //lowercase everything in given string to avoid counting uppercases as a separate char
+            var str = s.ToLower();
+
+            //use dictionary to store mappings of chars and appearance.
+            var map = new Dictionary<char, int>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                //add char to map if it does not contain the char and is alphabet
+                if (!map.ContainsKey(str[i]) && str[i] >= 'a' && str[i] <= 'z')
+                {
+                    map.Add(str[i], 1);
+                }
+                else
+                {
+                    map[str[i]] += 1;
+                }
+            }
+
+            //check length of dictionary to equal to 26 (26 letters in Alphabet)
+            if(map.Count != 26)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
