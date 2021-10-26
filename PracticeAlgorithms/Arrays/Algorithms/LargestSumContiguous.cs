5612341c9023f69655c7100fd375c1924624b330
Becky <becky.ruan524@gmail.com>
2021-10-25 20:46:20 -0700

    SubArr of equal 0 and 1; max cont subarr sum

---
+++ PracticeAlgorithms/Arrays/Algorithms/LargestSumContiguous.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays.Algorithms
+{
+    //find max sum of contiguous subarray (aka elements are next to e/o in arr)
+    public class LargestSumContiguous
+    {
+        public int MaxContiguousSubArrSum(int[] arr)
+        {
+            //initialize for Kadane's Algo
+            var maxGlobal = 0;
+            var maxAtIndex = 0;
+
+            //traverse array
+            for(int i = 0; i < arr.Length; i++)
+            {
+                //set max ending at current index = maxAtIndex + element at current index
+                maxAtIndex = maxAtIndex + arr[i];
+
+                //if global max is less than index max
+                //set global equal to max at index
+                if(maxGlobal < maxAtIndex)
+                {
+                    maxGlobal = maxAtIndex;
+                }
+
+                //if max at current index < 0
+                //set max at current index to 0
+                if(maxAtIndex < 0)
+                {
+                    maxAtIndex = 0;
+                }
+            }
+
+            return maxGlobal;
+        }
+    }
+}
---
+++ PracticeAlgorithms/Arrays/Algorithms/SubArrEqual1s0s.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays.Algorithms
+{
+    //arr of 0 and 1, count # of subarr with equal 0s and 1s
+    public class SubArrEqual1s0s
+    {
+        public int CountSubArrEqual1s0s(int[] arr)
+        {
+            var mapper = new Dictionary<int, int>();
+            var sum = 0;
+            var count = 0;
+
+            //traverse array and replace 0s with -1s
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] == 0)
+                {
+                    arr[i] = -1;
+                }
+
+                sum += arr[i];
+
+                //if sum is 0, implies that 0s and 1s are equal
+                //as -1 + 1 = 0
+                if(sum == 0)
+                {
+                    count++;
+                }
+
+                //if dictionary has sum, add count to value at sum
+                if (mapper.ContainsKey(sum))
+                {
+                    count += mapper[sum];
+                }
+
+                //otherwise, add sum to dictionary and count is 1
+                if (!mapper.ContainsKey(sum))
+                {
+                    mapper.Add(sum, 1);
+                }
+
+                //???
+                else
+                {
+                    var v = mapper[sum] + 1;
+                    mapper.Remove(sum);
+                    mapper.Add(sum, v);
+                }
+            }
+
+            return count;
+        }
+    }
+}
