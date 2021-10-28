9aca397c40cb345c6ce1877f143517565ec7f65f
Becky <becky.ruan524@gmail.com>
2021-10-27 20:06:56 -0700

    longest consecutive subseq; subarr sorted wave like

---
+++ PracticeAlgorithms/Arrays/Algorithms/LongestConsecSubSeq.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays.Algorithms
+{
+    //find length of longest sub-sequence of consecutive int
+    //order not important
+    public class LongestConsecSubSeq
+    {
+        public int LongestSubseq(int[] arr)
+        {
+            var hs = new HashSet<int>();
+            var result = 0;
+
+            //hash all elements of arr
+            for(int i = 0; i < arr.Length; i++)
+            {
+                hs.Add(arr[i]);
+            }
+
+            //check each possible seq from start
+            //update optimal length
+            for (int i = 0; i < arr.Length; i++)
+            {
+                //current element is starting element of sequence
+                if(!hs.Contains(arr[i] - 1))
+                {
+                    //check for next elements in seq
+                    var j = arr[i];
+                    while (hs.Contains(j))
+                    {
+                        j++;
+                    }
+
+                    //update optimal length if this length is more
+                    if(result < j - arr[i])
+                    {
+                        result = j - arr[i];
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ PracticeAlgorithms/Arrays/Algorithms/SortArrWave.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays.Algorithms
+{
+    //unsorted arr, sort into wave like arr
+    //arr[0] >= arr[1] <= arr[2]....
+    public class SortArrWave
+    {
+        //method to swap 2 ints
+        public void Swap(int[] arr, int a, int b)
+        {
+            var temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
+
+        public void WaveSortedArr(int[] arr)
+        {
+            //traverse even index of arr
+            for(int i = 0; i < arr.Length; i+= 2)
+            {
+                //current element < prev element
+                if(i > 0 && arr[i-1] > arr[i])
+                {
+                    Swap(arr, i - 1, i);
+                }
+
+                //current element < next element
+                if(i < arr.Length && arr[i] < arr[i + 1])
+                {
+                    Swap(arr, i, i + 1);
+                }
+            }
+        }
+    }
+}
