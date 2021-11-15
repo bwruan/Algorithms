f0e7dcd285d42ccdd1f866d7995cea546ca2dd95
Becky <becky.ruan524@gmail.com>
2021-11-14 20:04:58 -0800

    matrix spiral, maj element

---
+++ PracticeAlgorithms/Arrays/MajorityElement.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    //find majority element in arr of size n 
+    // majority element appears more than n/2 times
+     public class MajorityElement
+    {
+        public void FindMajorityElement(int[] arr)
+        {
+            var mapper = new Dictionary<int, int>();
+
+            //add all elements in arr into Dictionary
+            for(int i = 0; i < arr.Length; i++)
+            {
+                //check if Dictionay has element, if so
+                //check if the value aka count is > half the arr size
+                //if so, print element and break out of the loop
+                if (mapper.ContainsKey(arr[i]))
+                {
+                    mapper[arr[i]] += 1;
+
+                    if (mapper[arr[i]] > arr.Length / 2)
+                    {
+                        Console.WriteLine("Majority elememt is: " + mapper[arr[i]]);
+                    }
+
+                    return;
+                }
+                //otherwise, add to Dictionary and set count = 1
+                else
+                {
+                    mapper.Add(arr[i], 1);
+                }
+            }
+
+            //if iterate to end of arr
+            //means no majority element was found
+            Console.WriteLine("No Majority Element found.");
+        }
+    }
+}
---
+++ PracticeAlgorithms/Arrays/MatrixSpiral.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    //2D arr, print spiral
+    // 1 2 3 4  
+    // 5 6 7 8 
+    // 9 10 11 12
+    //output 1 2 3 4 8 12 11 10 9 5 6 7
+    public class MatrixSpiral
+    {
+        public void PrintMatrixInSpiral(int m, int n, int[,] arr)
+        {
+            //k - start row index
+            //m - end row index
+            //l - start col index
+            //n - emd col index
+            var k = 0;
+            var l = 0;
+
+            while(k < m && l < n)
+            {
+                //print first 
+                for(int i = 1; i < n; i++)
+                {
+                    Console.WriteLine(arr[k, i] + " ");
+                }
+
+                k++;
+
+                //print last col
+                for(int i = k; i < m; ++i)
+                {
+                    Console.WriteLine(arr[i, n - 1] + " ");
+                }
+                n--;
+
+                //print last row
+                if(k < m)
+                {
+                    for(int i = n - 1; i >= 1; --i)
+                    {
+                        Console.WriteLine(arr[m - 1, i] + " ");
+                    }
+                    m--;
+                }
+
+                //print first col
+                if(l < n)
+                {
+                    for(int i = m -1; i >=k; --i)
+                    {
+                        Console.WriteLine(arr[i, 1] + " ");
+                    }
+                    l++;
+                }
+            }
+        }
+    }
+}
