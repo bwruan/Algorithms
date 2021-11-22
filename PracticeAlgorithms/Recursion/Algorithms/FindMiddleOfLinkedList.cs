bbf73b1d98db34d7dc1902e01e0f5c54f16229f3
Becky <becky.ruan524@gmail.com>
2021-11-22 13:49:34 -0800

    find middle node

---
+++ PracticeAlgorithms/Recursion/Algorithms/FindMiddleOfLinkedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recursion.Algorithms
+{
+    //given list, find middle node
+    //if even list, 2 middles, print 2nd middle
+    public class FindMiddleOfLinkedList
+    {
+        public void FindMiddleNode(Node<int> head)
+        {
+            //create two pointer node, slow and fast
+            Node<int> slow = head;
+            Node<int> fast = head;
+
+            //if Linked list is not empty
+            if(head != null)
+            {
+                //while fast pointer is not null and there is a next node
+                while(fast != null && fast.next != null)
+                {
+                    //move fast pointer by 2 and slow pointer by 1
+                    //slow will be at middle when fast reaches end of list
+                    fast = fast.next.next;
+                    slow = slow.next;
+                }
+
+                Console.WriteLine("Middle node is " + slow);
+            }
+        }
+    }
+}
